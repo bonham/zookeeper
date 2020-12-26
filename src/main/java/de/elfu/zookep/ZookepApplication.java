e29c1a0676a6caae82a57df29ccafe00df6bbcd9
Art Furrer <2823246+bonham@users.noreply.github.com>
2020-12-26 22:43:34 +0100

    switch spring

---
+++ src/main/java/de/elfu/zookep/ZookepApplication.java
@@ -0,0 +1,13 @@
+package de.elfu.zookep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookepApplication.class, args);
+	}
+
+}
